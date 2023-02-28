5b6c1861a5ff7d42cbd98c77982e7eed774b9811
echamoina <saidchanfisittynouzlat@gmail.com>
2023-02-28 12:58:29 -0800

    3.task

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,5 +8,10 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 int _putchar(char c);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
 
 #endif
