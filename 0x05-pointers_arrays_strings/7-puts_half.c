d06811245d3f02973ad68242d3ba839704497232
echamoina <saidchanfisittynouzlat@gmail.com>
2023-02-28 14:33:46 -0800

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of a string
+ * @str: pointer to a string
+ * Return: void
+ */
+void puts_half(char *str)
+{
+int i, length;
+
+length = _strlen(str);
+for (i = (length + 1) / 2; i < length; i++)
+_putchar(str[i]);
+_putchar('\n');
+}
