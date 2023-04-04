3bc9628909d47e50455f317f8821b7981b906c2b
echamoina <echamoinaecha@gmail.com>
2023-04-04 16:16:10 -0700

    0.Task

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,7 +4,6 @@
 /**
  * print_listint - Prints all the elements of a listint_t list.
  * @h: pointer to the head of the list_t list.
- *
  * Return: number of nodes.
  */
 size_t print_listint(const listint_t *h)
